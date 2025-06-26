c7a852d8eae0f6e8edb15dff9d3b1842ec1ec01b
Andy-Silva05 <andy.s_1307@hotmail.com>
2025-06-26 16:32:08 -0500

    gclbhuiujoiok

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,3 @@
 import figuras.*;
 import ui.*;
 
-public class Main {
-    public static void main(String[] args) {
-        InterfazUsuario interfaz = new InterfazUsuario();
-        Object[] datosFiguras = interfaz.obtenerDatosFiguras();
-
-        CirculoDibujable circulo = (CirculoDibujable) datosFiguras[0];
-        RectanguloDibujable rectangulo = (RectanguloDibujable) datosFiguras[1];
-        TrianguloDibujable triangulo = (TrianguloDibujable) datosFiguras[2];
-
-        System.out.println("\nRESULTADOS:");
-        System.out.printf("Círculo - Área: %.2f, Perímetro: %.2f%n", circulo.getArea(), circulo.getPerimetro());
-        System.out.printf("Rectángulo - Área: %.2f, Perímetro: %.2f%n", rectangulo.getArea(), rectangulo.getPerimetro());
-        System.out.printf("Triángulo - Área: %.2f, Perímetro: %.2f%n", triangulo.getArea(), triangulo.getPerimetro());
-
-        VentanaDibujo ventana = new VentanaDibujo(circulo, rectangulo, triangulo);
-        ventana.mostrar();
-    }
-}

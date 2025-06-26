ca3aa6ea1431768496b64f5189ccf21d04817a8c
Andy-Silva05 <andy.s_1307@hotmail.com>
2025-06-26 17:08:12 -0500

    correccion

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,15 @@ public class Main {
         club.afiliarSocio("456", "Ana Torres", Tipo.VIP);
         club.afiliarSocio("789", "Carlos Ruiz", Tipo.VIP);
         club.afiliarSocio("321", "María López", Tipo.VIP);
-        club.afiliarSocio("999", "Extra VIP", Tipo.VIP); // No se afilia (máximo VIP = 3)
+        club.afiliarSocio("999", "Extra VIP", Tipo.VIP);
 
         club.agregarAutorizadoSocio("123", "Luis Amigo");
         club.agregarAutorizadoSocio("456", "Sofía Hermana");
 
-        club.registrarConsumo("123", "Juan Pérez", "Comida", 20000);         // permitido
-        club.registrarConsumo("123", "Luis Amigo", "Piscina", 10000);       // permitido
-        club.registrarConsumo("456", "Sofía Hermana", "Spa", 50000);        // permitido
-        club.registrarConsumo("456", "No Autorizado", "Bar", 20000);        // no se registra
+        club.registrarConsumo("123", "Juan Pérez", "Comida", 20000);
+        club.registrarConsumo("123", "Luis Amigo", "Piscina", 10000);
+        club.registrarConsumo("456", "Sofía Hermana", "Spa", 50000);
+        club.registrarConsumo("456", "No Autorizado", "Bar", 20000);
 
         System.out.println("\n Facturas de Juan Pérez:");
         for (Factura f : club.darFacturasSocio("123")) {
@@ -30,9 +30,9 @@ public class Main {
 
             System.out.println("\n ¿Se puede eliminar '123'? → " + club.sePuedeEliminarSocio("123"));
             System.out.println(" ¿Se puede eliminar '456'? → " + club.sePuedeEliminarSocio("456"));
-            System.out.println(" ¿Se puede eliminar '000'? → " + club.sePuedeEliminarSocio("000")); // No existe
+            System.out.println(" ¿Se puede eliminar '000'? → " + club.sePuedeEliminarSocio("000"));
 
-            club.eliminarAutorizadoSocio("456", "Sofía Hermana");  // tiene factura
+            club.eliminarAutorizadoSocio("456", "Sofía Hermana");
 
             club.eliminarAutorizadoSocio("123", "Luis Amigo");
 
